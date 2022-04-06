f741d3c5cfeb067cb4329683e2e75654ef56a994
Vince Skahan <vinceskahan@gmail.com>
2022-04-06 16:32:03 -0700

    usage

--- run-epaper.sh
+++ run-epaper.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/bash
 #
-source /home/pi/e-Paper/bin/activate && python3 /home/pi/e-Paper/RaspberryPi_JetsonNano/python/bin/epaper.py 
+
+# this assumes nohup.out 'here' is a symlink to /dev/null
+cd /home/pi/e-Paper/RaspberryPi_JetsonNano/python/bin
+
+# activate the pyenv and nohup the script
+source /home/pi/e-Paper/bin/activate && nohup python3 /home/pi/e-Paper/RaspberryPi_JetsonNano/python/bin/epaper.py &
+
+# alternately run the system python if it has had the requirements installed
+#nohup python3 /home/pi/e-Paper/RaspberryPi_JetsonNano/python/bin/epaper.py &
 
